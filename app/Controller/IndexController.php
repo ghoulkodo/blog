460bc0732f8d26da35ba0fc9c0785864da109613
宋江 <tianye263@gmail.com>
2017-02-09 20:30:05 +0800

    主页

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -2,24 +2,19 @@
 namespace Controller;
 use Controller\Controller;
 use Controller\TopicController;
+use Framework\Paginate;
 class IndexController extends Controller
 {
+	
 	public function __construct(){
 		parent::__construct();
 	}
 
 	public function index()
 	{
-		if (empty($_GET['page'])) {
-			$page = 1;
-		} else {
-			$page = $_GET['page'];
-		}
-
+		
 		$articles = new TopicController();
-		$data= $articles->articles($page);
-		$this->assign('title' , 'Sung Chiang');
-		$this->assign('topic' , $data);
-		$this->display('index.html');
+		$data= $articles->articles();
+
 	}
 }
