7c5894f80e96b809e2ec430a3b6a0d83cae47cc8
宋江 <tianye263@gmail.com>
2017-02-09 20:31:24 +0800

    用户模块

--- app/Model/UserModel.php
+++ app/Model/UserModel.php
@@ -4,17 +4,27 @@ use Framework\Model;
 
 class UserModel extends Model
 {
+	//添加用户
 	public function doAdd($data)
 	{
 		$result = $this->add($data);
 		return $result;
 	}	
 
-	//执行查询
 	
+	//执行查询
 	public  function doFind($data)
 	{
 		$result = $this->where("username = '{$data['username']}' and password='{$data['password']}'")->select();
 		return $result;
 	}
+
+	//屏蔽用户
+	public function doBan($id){
+
+		$result = $this->where("uid = $id")->update();
+		return $result;
+
+	}
+
 }
