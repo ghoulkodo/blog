972de9d03b29b4617ab898fd3e952219faa1bd48
宋江 <tianye263@gmail.com>
2017-02-09 20:31:16 +0800

    文章模块

--- app/Model/TopicModel.php
+++ app/Model/TopicModel.php
@@ -1,13 +1,26 @@
 <?php
 namespace Model;
 use Framework\Model;
+use Framework\Parsedown;
 
 class TopicModel extends Model
 {
 	//分页查询文章内容
-	public function getTopic($page){
-		$start = ($page-1)*6;
-		$result = $this->limit("$start,6")->select();
+	public function getTopic($page , $offset){
+		$start = ($page-1)*$offset;
+		$result = $this->limit("$start,$offset")->select();
+		return $result;
+	}
+
+	//查看文章详情
+	public function viewTopic($id){
+		$result = $this->where("aid = $id")->select();
+		return $result;
+	}
+
+	//站点文章统计
+	public function getCount(){
+		$result = $this->count();
 		return $result;
 	}
 
@@ -27,4 +40,21 @@ class TopicModel extends Model
 	public function udtTopic($data){
 		
 	}
+
+	//获取回复内容
+	public function getRec(){
+
+	}
+
+	//添加回复
+	public  function addRec(){
+
+	}
+
+	//删除回复
+	public function delRec(){
+
+	}
+
+	
 }
