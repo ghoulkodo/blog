d34f515feae9a3012452bf704ffa94eba8e41b0a
宋江 <tianye263@gmail.com>
2017-02-10 20:54:06 +0800

    回复控制器

--- app/Controller/RecController.php
+++ app/Controller/RecController.php
@@ -1,2 +1,43 @@
 <?php
+namespace Controller;
+use Controller\TopicController;
+use Controller\UserController;
+use Framework\Model;
+use Framework\Parsedown;
 
+#
+#	评论操作类
+#
+class CommController extends Controller
+{
+
+	public function __construct(){
+		parent::__construct();
+
+		$this->comm = new CommModel();
+	}
+	//新建回复
+
+	//列出回复
+	public function list($id){
+
+		$result = $this->Comm->getComm($id);
+
+		$data = self::parseComm($data);
+
+		return $data;
+		
+	}
+	public static function parseComm($data){
+
+		//
+
+		foreach ($data as $key => $value) {
+			if (!empty($value['rere'])) {
+				
+			}
+		}
+	}
+	//审核回复
+
+}
