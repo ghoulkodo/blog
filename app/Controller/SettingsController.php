3a2ec1e42cd04b03300154bdd8ca438d33933051
宋江 <tianye263@gmail.com>
2017-02-10 20:54:19 +0800

    后台控制器

---
+++ app/Controller/SettingsController.php
@@ -0,0 +1,18 @@
+<?php
+namespace Controller;
+
+use Controller\Controller;
+use Controller\TopicController;
+use Controller\UserController;
+
+#
+#	后台控制器
+#
+class SettingsController
+{
+	//后台入口
+	public function me(){
+		echo 111;
+	}
+
+}
