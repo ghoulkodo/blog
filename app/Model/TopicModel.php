0e09e531d419bd7caebe4ae151bcedd2564a6b37
宋江 <tianye263@gmail.com>
2017-02-10 20:55:41 +0800

    整理 文章模块

--- app/Model/TopicModel.php
+++ app/Model/TopicModel.php
@@ -3,57 +3,51 @@ namespace Model;
 use Framework\Model;
 use Framework\Parsedown;
 
+#	文章模块类
+
 class TopicModel extends Model
 {
-	//分页查询文章内容
+	//分页查询文章索引
 	public function getTopic($page , $offset){
 		$start = ($page-1)*$offset;
 		$result = $this->limit("$start,$offset")->select();
+
 		return $result;
 	}
 
 	//查看文章详情
 	public function viewTopic($id){
 		$result = $this->where("aid = $id")->select();
+		
 		return $result;
 	}
 
 	//站点文章统计
 	public function getCount(){
 		$result = $this->count();
+
 		return $result;
 	}
 
-	//删除文章
-	public function delTopic($id){
-		$result = $this->set('display = 0')->where("aid = $id")->update();
+	//编辑管理文章/
+	public function edtTopic($id , $isdel = 1 , $data){
+		//删除
+		if ($isdel == 1) {
+			$result = $this->set('display = 0')->where("aid = $id")->update();
+			return $result;
+		}
+		//修改
+		$result = $this->set($data)->where("aid = $id")->update();
+
 		return $result;
+
 	}
 
 	//添加文章
-	public function addTopic($data){
-		
-	}
+	public function newTopic($data){
+		$result = $this->add($data);
 
-
-	//更改文章
-	public function udtTopic($data){
-		
-	}
-
-	//获取回复内容
-	public function getRec(){
-
-	}
-
-	//添加回复
-	public  function addRec(){
-
-	}
-
-	//删除回复
-	public function delRec(){
-
+		return $result;
 	}
 
 	
