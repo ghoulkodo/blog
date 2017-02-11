1581c86d9af73eaf91817afb156292bf02d6b626
宋江 <tianye263@gmail.com>
2017-02-11 17:24:34 +0800

    留言model

---
+++ app/Model/CommModel.php
@@ -0,0 +1,44 @@
+<?php
+namespace Model;
+use Framework\Model;
+use Framework\Comment;
+
+#	留言模块
+
+class CommModel extends Model
+{
+
+	public function __construct(){
+		parent::__construct();
+		$this->comm = new Comment();
+	}
+
+	//查看指定id的所有回复
+	public function getComm($id){
+
+		
+		$arr = $this->where("aid = $id")->order('ctime desc')->limit('5')->select();
+		$data = $this->comm->find_parent($arr);
+		return $data;
+	}
+
+	//查看引用回复
+	public function reComm($parentid){
+
+	}
+
+	//创建新回复
+	public function newComm($data){
+		$result = $this->add($data);
+
+		return $result;
+	}
+
+	//删除回复（审核回复）
+	public function chkComm($id){
+		$result = $this->set("display = 0")->update($id);
+
+		return $result;
+	}
+
+}
